1edbeef52319e0e05d6aea1892361b936a253181
Z <kerenyi.nagy.zoltan@gmail.com>
2024-07-11 14:24:48 +0200

    Update download-srtm-data.sh

--- download-srtm-data.sh
+++ download-srtm-data.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+
+
 set -eu
-set -x
+
 WORKDIR='/code/data'
 
 file1='SRTM_NE_250m_TIF'
@@ -9,19 +11,20 @@ file3='SRTM_W_250m_TIF'
 
 cd "$WORKDIR"
 
-if find /code/data/ -name "*.rar" ;then
-    echo "[*] tile data files (RAR) have already been downloaded, exit"
+if [ -f "$file1.rar" ] || [ -f "$file2.rar" ] || [ -f "$file3.rar" ] ;then
+    echo "[*] SRTM data files (RAR) have already been downloaded"
     exit 0
-
-elif find /code/data/ -name "*.tif" ;then
-    echo "[*] tile data files (TIF) have already been downloaded and extracted, exit"
-    exit 0
-else
-    echo '[+] Downloading SRTM data files'
-    wget "https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/$file1.rar" && unar -f "$file1.rar" && rm -rf "$file1.rar"
-    wget "https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/$file2.rar" && unar -f "$file2.rar" && rm -rf "$file2.rar"
-    wget "https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/$file3.rar" && unar -f "$file3.rar" && rm -rf "$file3.rar"
 fi
+
+if [ -f "$file1.tif" ] || [ -f "$file2.tif" ] || [ -f "$file3.tif" ] ;then
+    echo "[*] SRTM data files (TIF) have already been downloaded and extracted"
+    exit 0
+fi 
+
+echo '[+] Downloading SRTM data files'
+wget "https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/$file1.rar" && unar -f "$file1.rar" && rm -rf "$file1.rar"
+wget "https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/$file2.rar" && unar -f "$file2.rar" && rm -rf "$file2.rar"
+wget "https://srtm.csi.cgiar.org/wp-content/uploads/files/250m/$file3.rar" && unar -f "$file3.rar" && rm -rf "$file3.rar"
 
 echo "[+] Done"
 
